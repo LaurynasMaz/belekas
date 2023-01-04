4d280c91c4f85d88b6cae631f5ef0d0b8b06b1fe
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-01-04 14:16:42 +0200

    korteles

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+fetch("./data/data.json")
+  .then(response => response.json())
+  .then(data => {
+    data.video.forEach(video => {
+      const card = document.createElement('div');
+      card.classList.add('video');
+      card.innerHTML = `
+        <div class="thumbnail">
+          <img src="${video.image}" />
+        </div>
+          <div class="details">
+            <div class="author">
+              <img src="${video.author}" alt="" />
+            </div>
+            <div class="title">
+            <h3>${video.title}</h3>
+            </div>
+            <div class="authorName">
+            <a>${video.name}</a>
+            </div>
+            <div class="additionalInfo">
+            <span>${video.info}</span>
+            </div>
+        </div>
+      `;
+      const container = document.querySelector('#content .videos');
+      container.append(card);
+    });
+  })
